51cdb8478b3d9141fa8ef6bf6f725f0fd4122ed6
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-02-05 15:22:46 +0000

    footer github link change

--- src/Footer.js
+++ src/Footer.js
@@ -1,31 +1,33 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
 
 export default function Footer() {
-    return (
-        <div class="container source">
-            <p>
-                This project was coded by{' '}
-                <a
-                    href="https://linkedin.com/in/carolinamacdian"
-                    class="link"
-                    target="_blank">
-                    Carolina Macdian
-                </a>
-                , is{' '}
-                <a
-                    href="https://github.com/CarolinaMacdian/JsVanilla_Weather-app"
-                    class="link"
-                    target="_blank">
-                    open-sourced on GitHub
-                </a>{' '}
-                and{' '}
-                <a
-                    href="https://inspiring-parfait-3823c8.netlify.app/"
-                    class="link"
-                    target="_blank">
-                    hosted on Netlify
-                </a>
-            </p>
-        </div>
-    );
+  return (
+    <div class='container source'>
+      <p>
+        This project was coded by{" "}
+        <a
+          href='https://linkedin.com/in/carolinamacdian'
+          class='link'
+          target='_blank'>
+          Carolina Macdian
+        </a>
+        , is{" "}
+        <a
+          href='https://github.com/CarolinaMacdian/weather-react'
+          class='link'
+          target='_blank'>
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href='https://inspiring-parfait-3823c8.netlify.app/'
+          class='link'
+          target='_blank'>
+          hosted on Netlify
+        </a>
+      </p>
+    </div>
+  );
 }
