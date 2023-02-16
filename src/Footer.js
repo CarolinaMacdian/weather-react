69a1176cba6f250a0b274d565e9aeb99947f152d
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-02-16 15:12:35 +0000

    links fixed

--- src/Footer.js
+++ src/Footer.js
@@ -10,21 +10,24 @@ export default function Footer() {
         <a
           href='https://linkedin.com/in/carolinamacdian'
           class='link'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer'>
           Carolina Macdian
         </a>
         , is{" "}
         <a
           href='https://github.com/CarolinaMacdian/weather-react'
           class='link'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer'>
           open-sourced on GitHub
         </a>{" "}
         and{" "}
         <a
           href='https://inspiring-parfait-3823c8.netlify.app/'
           class='link'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer'>
           hosted on Netlify
         </a>
       </p>
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,17 +1,19 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
 
 export default function Temperature() {
-    return (
-        <p className="col-7 text-center">
-            <strong className="temperature">10</strong>
-            <span className="grau"> º</span>
-            <span className="units">
-                <a href="#">C</a>
-                <span className="bar">|</span>
-                <a href="#">F</a>
-            </span>
-            <br />
-            <span className="discription">Cloudy</span>
-        </p>
-    );
+  return (
+    <p className='col-7 text-center'>
+      <strong className='temperature'>10</strong>
+      <span className='grau'> º</span>
+      <span className='units'>
+        <p>C</p>
+        <span className='bar'>|</span>
+        <p>F</p>
+      </span>
+      <br />
+      <span className='discription'>Cloudy</span>
+    </p>
+  );
 }
